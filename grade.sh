49c82673454bc2abdb507a83d0722aafa135d1b8
MatthewK0617 <kimmatthew2022@gmail.com>
2023-03-01 15:48:17 -0800

    initial commit

--- grade.sh
+++ grade.sh
@@ -3,3 +3,22 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+file=student-submission/ListExamples.java
+
+if [[ ! -f $file ]]
+then
+	exit
+fi
+
+cp $file ss.java
+javac -cp $CPATH *.java
+
+if [[ $? -ne 0 ]]
+then
+	exit $?
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples | grep -h "Failure" > out.txt
+
+cat out.txt
